0f18068ae4d6fc47af2a11bf73457af40fcc4313
mich <mich-cook@users.noreply.github.com>
2020-09-11 01:26:00 -0700

    fix a bunch of style inconsistencies

--- microservices/exercise-tracker/server.js
+++ microservices/exercise-tracker/server.js
@@ -1,14 +1,14 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-const cors = require('cors')
+const cors = require('cors');
 
 const mongo = require('mongodb');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log(`Database connection complete`))
@@ -27,15 +27,15 @@ const exerciserSchema = new mongoose.Schema({
 
 const Exerciser = mongoose.model('Exerciser', exerciserSchema);
 
-app.use(cors())
+app.use(cors());
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 
@@ -80,7 +80,7 @@ app.post("/api/exercise/add", (req, res) => {
   }
 
   let workout = { "description": description, "duration": duration };
-  if ( req.body.date !== undefined) { workout.date = req.body.date }
+  if ( req.body.date !== undefined) { workout.date = req.body.date; }
 
   let update = { $push: { "log": workout }};
 
@@ -170,30 +170,30 @@ app.get("/api/exercise/log/", (req, res) => {
 
 // Not found middleware
 app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
+  return next({status: 404, message: 'not found'});
+});
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
-  let errCode, errMessage
+  let errCode, errMessage;
 
   if (err.errors) {
     // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
     // report the first validation error
-    errMessage = err.errors[keys[0]].message
+    errMessage = err.errors[keys[0]].message;
   } else {
     // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
+    errCode = err.status || 500;
+    errMessage = err.message || 'Internal Server Error';
   }
   res.status(errCode).type('txt')
-    .send(errMessage)
-})
+    .send(errMessage);
+});
 
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log('Your app is listening on port ' + listener.address().port);
+});
